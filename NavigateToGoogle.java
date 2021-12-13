bd583ffe8568e73a32057ac57fe159af6c58233e
Garima Sharma <96081651+garimasharma98@users.noreply.github.com>
2021-12-14 01:03:16 +0530

    Update NavigateToGoogle.java

--- NavigateToGoogle.java
+++ NavigateToGoogle.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -14,33 +13,44 @@ import utilities.selenium.WebDriverManager;
 
 public class NavigateToGoogle {
 	
+	
 	WebDriver driver;
 	WebDriverManager webDrvMgr;
 	
 	@BeforeClass
 	public void setUp(){
-		webDrvMgr= new WebDriverManager();
+		webDrvMgr = new WebDriverManager();
 		driver = webDrvMgr.launchBrowser("chrome");
-		driver.get("https://www.google.co.in");
+		driver.get("http://www.google.co.in");
 	}
 	
 	@Test
-	public void navigateToGoogle() {
-		String title= driver.getTitle();
-		Assert.assertEquals("Google", title);
-		String url= driver.getCurrentUrl();
-		Assert.assertEquals("https://www.google.co.in/", url);
+	public void navigateToGoogleTest(){
+		String title = driver.getTitle();
+		System.out.println(title);
+		
+		String url = driver.getCurrentUrl();
+		System.out.println(url);
+		
 		WebElement oelem = driver.findElement(By.cssSelector("div.uU7dJb"));
 		String innerText = oelem.getText();
-		Assert.assertEquals("India", innerText);
-		String attribClass = oelem.getAttribute("Class");
-		Assert.assertEquals("uU7dJb", attribClass);
+		System.out.println(innerText);
+
+		String attribClass = oelem.getAttribute("class");
+		System.out.println(attribClass);
+	
 		
+		List<WebElement> oList = driver.findElements(By.cssSelector("div.KxwPGc.AghGtd>*"));
+		for(WebElement oElem: oList){
+			String innertext = oElem.getText();
+			System.out.println(innertext);
+		}
 	}
 	
 	@AfterClass
-	public void tearDown() {
+	public void tearDown(){
 		driver.quit();
 	}
+
 
 }
